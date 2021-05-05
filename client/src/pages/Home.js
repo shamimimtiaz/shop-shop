51fdfbbfea2ce9f9227d62104d61f047278db048
unknown <km_si@ymail.com>
2021-05-05 13:55:59 -0400

    fixing some code for using global state

---
+++ client/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+
+// const Home = () => {
+//   const [currentCategory, setCategory] = useState("");
+
+//   return (
+//     <div className="container">
+//       <CategoryMenu setCategory={setCategory} />
+//       <ProductList currentCategory={currentCategory} />
+//     </div>
+//   );
+// };
+
+
+const Home = () => {
+  return (
+    <div className="container">
+      <CategoryMenu />
+      <ProductList />
+    </div>
+  );
+};
+
+export default Home;
