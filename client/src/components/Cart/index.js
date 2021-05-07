f194d51144b6700f111db8446c86468fa98b49eb
unknown <km_si@ymail.com>
2021-05-06 23:13:14 -0400

    add checkout functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,10 +4,27 @@ import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
 import './style.css';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+//The data variable will contain the checkout session, but only after the query is called with the getCheckout() function.
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+      if (data) {
+        stripePromise.then((res) => {
+          res.redirectToCheckout({ sessionId: data.checkout.session });
+        });
+      }
+    }, [data]);
+
+
     useEffect(() => {
       async function getCart() {
         const cart = await idbPromise('cart', 'get');
@@ -29,6 +46,18 @@ function calculateTotal() {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+  function submitCheckout() {
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds }
+    });
   }
 
 if (!state.cartOpen) {
@@ -55,7 +84,7 @@ if (!state.cartOpen) {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
+            <button onClick={submitCheckout}>
               Checkout
             </button>
             :
